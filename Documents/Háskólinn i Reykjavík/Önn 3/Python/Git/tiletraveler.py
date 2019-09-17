aa174abc788a01d207ca15a4dea33e3b17395d70
gudrunb18 <55437746+gudrunb18@users.noreply.github.com>
2019-09-17 10:33:18 +0000

    Update tiletraveler.py

--- Documents/Háskólinn i Reykjavík/Önn 3/Python/Git/tiletraveler.py
+++ Documents/Háskólinn i Reykjavík/Önn 3/Python/Git/tiletraveler.py
@@ -1,12 +1,11 @@
-x,y = 1,1 #the starting point
+x,y = 1,1
 
-#a function for every direction possible
-def north(x,y):
+def north(x,y): 
     if y == 3 or (x,y) == (2,2) or (x,y) == (3,1):
         return False
     return True
 
-def east(x,y):
+def east(x,y): 
     if (x,y) != (1,2) and (x,y) != (1,3) and (x,y) != (2,3):
         return False
     return True
@@ -21,7 +20,6 @@ def south(x,y):
         return False
     return True
 
-#a function that combines the direction and if the direction is infact possible
 def string(n,e,s,w):
     direction = ""
     while direction == "":
@@ -40,8 +38,7 @@ def string(n,e,s,w):
     if w == True and direction != "(W)est":
         direction += " or (W)est"
     return direction
-
-#a function that allows the user to input n instead of without making a difference
+    
 def command_call():
     i = input("Direction: ")
     i = i.upper()
@@ -51,7 +48,6 @@ def translocator(boolean):
     if boolean == True:
         return 1
     return 0
-
 
 def move(x,y,string):
     if string == "N":
